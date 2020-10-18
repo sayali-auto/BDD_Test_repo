a09da16dc71d16ccd4f299ef0441926ae4fef8f3
Nikhil <demomail963@gmail.com>
2020-10-18 14:15:56 +0530

    initial project push

---
+++ git/src/main/java/testgit/git/App.java
@@ -0,0 +1,13 @@
+package testgit.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
